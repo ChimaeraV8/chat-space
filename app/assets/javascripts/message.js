31330ea96df79609ced0c1ba2168a2b620784fc4
ChimaeraV8 <macshirious@icloud.com>
2020-02-05 21:52:57 +0900

    Update message.js

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -85,7 +85,6 @@ $(function(){
     });
   };
   
-  setInterval(reloadMessages, 7000);
   if (document.location.href.match(/\/groups\/\d+\/messages/)) {
     setInterval(reloadMessages, 7000);
   }
